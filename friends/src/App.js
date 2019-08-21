3204605234bd90781b11e4307df3ac299221a9a7
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-21 11:37:38 -0400

    PrivateRoute

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+      </div>
+    </Router>
   );
 }
 
