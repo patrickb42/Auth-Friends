77bd54ced3c56095ae97f70a7bc81211ff9f52f7
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-21 12:52:15 -0400

    logging in indicator

--- friends/src/components/Login.jsx
+++ friends/src/components/Login.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 
 import LoginForm from './LoginForm';
 
 const Login = (props) => {
+  const [loggingIn, setLoggingIn] = useState(false);
   return (<>
     {(localStorage.getItem('token'))
       ? <Redirect to="/" />
-      : <LoginForm />}
+      : <LoginForm setLoggingIn={setLoggingIn} />}
+    {loggingIn && "Logging In..."}
   </>);
 };
 
